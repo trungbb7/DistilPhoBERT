40fdaf7a869c7cfbbfb36fbb4be91489fbf8153a
Minh Trung Nguyen <trungbb7@gmail.com>
2026-03-20 18:29:28 +0700

    update data processing pipeline

--- data_processing_pipeline.py
+++ data_processing_pipeline.py
@@ -1,8 +1,11 @@
+import os
 import re
 import unicodedata
 import hashlib
 import sqlite3
-from datasets import load_dataset
+from datasets import load_dataset, Dataset
+import pyarrow as pa
+import pyarrow.parquet as pq
 from bs4 import BeautifulSoup
 
 # CLEANING FUNCTIONS
@@ -44,7 +47,7 @@ def normalize_whitespace(text):
 
 
 def clean_text(example):
-    text = example["text"]
+    text = example["content"]
 
     text = remove_html(text)
     text = normalize_unicode(text)
@@ -79,3 +82,58 @@ def is_duplicated(text):
 
 def deduplicate(example):
     return not is_duplicated(example["text"])
+
+
+# Load dataset
+dataset = load_dataset(
+    "parquet", data_files="datasets/temp/*.parquet", split="train", streaming=True
+)
+
+# small_dataset = dataset.take(10)
+
+
+# Apply pipeline
+
+# 1. Clean text
+print("Cleaning text")
+dataset = dataset.map(clean_text, remove_columns=dataset.column_names)
+
+# 2. Filter
+print("Filter text")
+dataset = dataset.filter(filter_length)
+
+# 3. Dedup
+print("Dedup text")
+dataset = dataset.filter(deduplicate)
+
+# Save Output
+print("Save output")
+output_path = "datasets/temp_output/out.parquet"
+batch_size = 10000
+
+buffer = []
+writer = None
+
+for example in dataset:
+    buffer.append({"text": example["text"]})
+    if len(buffer) >= batch_size:
+        table = pa.Table.from_pylist(buffer)
+
+        if writer is None:
+            writer = pq.ParquetWriter(output_path, table.schema)
+        writer.write_table(table)
+        buffer = []
+
+if buffer:
+    table = pa.Table.from_pylist(buffer)
+    if writer is None:
+        writer = pq.ParquetWriter(output_path, table.schema)
+        writer.write_table(table)
+if writer:
+    writer.close()
+
+
+# Cleaning steps
+cursor.close()
+conn.close()
+os.remove("dedup.db")
